36b42a4063781b80ac82b6051a063fc874db20e4
Cat McLoughlin <catmclough@gmail.com>
2021-01-10 18:37:51 -0800

    add more photos and random width and rotation for each

--- src/components/Japan.js
+++ src/components/Japan.js
@@ -6,9 +6,11 @@ export default function Japan() {
   return (
     <>
       <Header text="Japan" />
-      <div class="photo-section">
+      <div className="photo-section">
         <Photo src="/japan/tsukiji.JPG" alt="Tsukiji Market Fish Vendor" />
         <Photo src="/japan/sushi-breakfast.JPG" alt="Emma eating at a Tsukiji sushi restaurant" />
+        <Photo src="/japan/chips.PNG" alt="Chips through the eyes of a bad translation app" />
+        <Photo src="/japan/piss-alley.JPG" alt="Me walking through 'Piss Alley'. Gross name, cool place." />
       </div>
     </>
   );
